b2d20e456d790be50bee57fad46f595b8141460f
Thomas Tallaksen <thomastallaksen@gmail.com>
2019-09-03 08:11:18 +0200

    Oppdatert med nye NHO-tall

--- Kvalitet.R
+++ Kvalitet.R
@@ -3,36 +3,40 @@ library(tidyverse)
 library(ggpubr)
 
 #Importerer bakgrunnstallene fra NHOanalysen og tall på privatisering fra SSB 12367
-dea <- read_excel("/Users/thomastallaksen/Documents/R/Prosjekter/Kvalitet19/190702_dea-analyse-resultat-og-input.xlsx")%>%
+dea <- read_excel("/Users/thomastallaksen/Documents/R/Prosjekter/Kvalitet19/190830_dea-analyse-resultat-og-input.xlsx")%>%
   mutate(Kommunenr = str_pad(Kommunenr, width=4, side="left", pad="0"))
 ssb <- read_excel("/Users/thomastallaksen/Documents/R/Prosjekter/Kvalitet19/KOSbelop0000.xlsx")
-ssb$`2017` <- as.numeric(ssb$`2017`)
+ssb$Beløp <- as.numeric(ssb$Beløp)
 
-per_funksjon <- ssb%>%
-  filter(Art_nr %in% c("A370", "AGD4"))%>%
-  group_by(Kommunenummer, Kommunenavn, Funksjon, Art)%>%
-  summarise(Kostnad = sum(`2017`, na.rm = TRUE))%>%
-  ungroup()%>%
-  group_by(Kommunenummer, Kommunenavn, Funksjon)%>%
-  mutate(Andel = round(Kostnad/sum(Kostnad)*100, digits = 1))
 
 samlet <- ssb%>%
-  filter(Art_nr %in% c("A370", "AGD4"))%>%
-  group_by(Kommunenummer, Kommunenavn, Art)%>%
-  summarise(Kostnad = sum(`2017`, na.rm = TRUE))%>%
-  ungroup()%>%
-  group_by(Kommunenummer, Kommunenavn)%>%
+  filter(Artikkelnummer %in% c("AG2", "A370", "AGD4", "A590"))%>%
+  filter(Funksjon != "Institusjonslokaler" | Artikkelnummer != "A370")%>%
+  group_by(Kommunenummer, Kommune, Funksjon, Artikkelnummer, Artikkel)%>%
+  summarise(Kostnad = sum(Beløp, na.rm = TRUE))%>%  
+  ungroup()%>%   
+  mutate(Deler = ifelse(Artikkelnummer %in% c("A370", "AG2"), "Kjøp", "Kostnader"))%>%
+  mutate(Kostnad = ifelse(Artikkelnummer == "A590", -Kostnad, Kostnad))%>%
+  group_by(Kommunenummer, Kommune, Deler)%>%
+  summarise(Kostnad = sum(Kostnad))%>%
   mutate(Andel = round(Kostnad/sum(Kostnad)*100, digits = 1))%>%
-  filter(Art == "Kjøp fra andre (private)")%>%
-  select(Kommunenummer, Kommunenavn, Andel)
+  filter(Deler == "Kjøp")
 
 sammenkoblet <- dea%>%
   select(Kommunenr, `Kommune fullt navn`, `Score, kvalitet`)%>%
+  left_join(samlet, by = c("Kommunenr" = "Kommunenummer"))%>%
+  mutate(Snitt = ifelse(Andel > 6.5, "Over", "Under"))%>%
+  na.omit()%>%
+  group_by(Snitt)%>%
+  summarise("Antall kommuner" = n(), Snittkvalitet = round(mean(`Score, kvalitet`), digits = 2), Standardavvik = sd(`Score, kvalitet`))
+
+graf <- dea%>%
+  select(Kommunenr, `Kommune fullt navn`, `Score, kvalitet`)%>%
   left_join(samlet, by = c("Kommunenr" = "Kommunenummer"))
 
-ggplot(sammenkoblet, aes(x = `Score, kvalitet`, y = Andel))+
+ggplot(graf, aes(y = `Score, kvalitet`, x = Andel))+
   geom_point()+
-  ylim(0,40)+
+  xlim(0,40)+
   geom_smooth(method='lm')+
   stat_cor(method = "pearson", label.x = 25, label.y = 30)
 
